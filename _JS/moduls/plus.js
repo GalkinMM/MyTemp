b70e073e2c83312dd8f3a2bfbde7a2ed2d6d473f
Galkin Maxim <maxim.galkin.1991@gmail.com>
2022-06-03 15:41:37 +0300

    changes
    changes

--- _JS/moduls/plus.js
+++ _JS/moduls/plus.js
@@ -3,32 +3,39 @@ const anyData = 'Любой тип данных'
 
 // Консоль
 
-/** 
- * Функция printC(data) заменяет функцию console.log(data) для упрощения записи
- * @type {anyData} */
-const printC = console.log;
+    /** 
+     * Функция printC(data) заменяет функцию console.log(data) для упрощения записи
+     * @type {anyData} */
+    const printC = console.log;
 
 
 // Функции проверки
 
-/**
- * Функция проверяет, является ли переменная числом
- * @param {anyData} data
- * @return {Boolean}
- */
-const isNumber = data => {
-    return (typeof(data) === 'number' && !isNaN(data));
-};
+    /**
+     * Функция проверяет, является ли переменная числом
+     * @param {anyData} data
+     * @return {Boolean}
+     */
+    const isNumber = data => {
+        return (typeof(data) === 'number' && !isNaN(data))
+    }
+
+    //Нужно ли это?
+    const isPositive = data => {
+        if (isNumber(data) && data > 0) {
+            return true}
+        else {return false}
+    }
 
 // info()
-const info = data => {
-    switch(true) {
-        case isNumber(data):
-            printC({
-                data: data, 
-                type: "Number",
-                // Доработать
-            });
-            break;
-    }
-};
+    const info = data => {
+        switch(true) {
+            case isNumber(data):
+                printC({
+                    data: data, 
+                    type: "Number",
+                    // Доработать
+                });
+                break;
+        }
+    };
